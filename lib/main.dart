11a8ee6e4e7842c4f141bc66a2f0cfeeaa51190a
Vinicius <viniciusmvn@pm.me>
2021-06-23 06:18:25 -0300

    chore(base-project): add base project structure

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(PayFlow());
+}
+
+class PayFlow extends StatelessWidget {
+  const PayFlow({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
