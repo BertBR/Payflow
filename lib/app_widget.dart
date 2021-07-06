627582bc4d9d49a832dfd729e7902f7dcc7dd0d7
Vinicius <viniciusmvn@pm.me>
2021-07-06 10:01:41 -0300

    chore(google-signin): add google signin dependencies

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/modules/login/login_page.dart';
 
 import 'modules/splash/splash_page.dart';
 import 'shared/themes/app_colors.dart';
@@ -9,7 +10,7 @@ class AppWidget extends StatelessWidget {
     return MaterialApp(
       title: 'Pay Flow',
       theme: ThemeData(primaryColor: AppColors.primary),
-      home: SplashPage(),
+      home: LoginPage(),
     );
   }
 }
