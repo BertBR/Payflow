6e918672cccc4d90ba07db453bf5aa52f2661212
Vinicius <viniciusmvn@pm.me>
2021-07-10 11:12:47 -0300

    feat(named-routes): to ease app navigation

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:payflow/modules/login/login_page.dart';
 
+import 'modules/home/home_page.dart';
 import 'modules/splash/splash_page.dart';
 import 'shared/themes/app_colors.dart';
 
@@ -10,7 +11,12 @@ class AppWidget extends StatelessWidget {
     return MaterialApp(
       title: 'Pay Flow',
       theme: ThemeData(primaryColor: AppColors.primary),
-      home: LoginPage(),
+      initialRoute: "/splash",
+      routes: {
+        '/splash': (context) => SplashPage(),
+        '/home': (context) => HomePage(),
+        '/login': (context) => LoginPage()
+      },
     );
   }
 }
