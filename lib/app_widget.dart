c4337e9418c3892a92b6275ea9d2b078b1438a8a
Vinicius <viniciusmvn@pm.me>
2021-07-13 07:59:56 -0300

    feat(barcode-scanner): add barcode scanner screen

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:payflow/modules/barcode_scanner/barcode_scanner_page.dart';
+import 'package:payflow/modules/insert_billet/insert_billet_page.dart';
 import 'package:payflow/modules/login/login_page.dart';
 
 import 'modules/home/home_page.dart';
@@ -6,16 +9,26 @@ import 'modules/splash/splash_page.dart';
 import 'shared/themes/app_colors.dart';
 
 class AppWidget extends StatelessWidget {
+  AppWidget() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.portraitUp,
+    ]);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Pay Flow',
-      theme: ThemeData(primaryColor: AppColors.primary),
+      theme: ThemeData(
+          primarySwatch: Colors.orange, primaryColor: AppColors.primary),
       initialRoute: "/splash",
       routes: {
         '/splash': (context) => SplashPage(),
         '/home': (context) => HomePage(),
-        '/login': (context) => LoginPage()
+        '/login': (context) => LoginPage(),
+        '/barcode_scanner': (context) => BarcodeScannerPage(),
+        '/insert_billet': (context) => InsertBilletPage()
       },
     );
   }
